042ffedf3d5ed811fa70bce6a9c3ca953e805563
Takahiro55555 <tkhr.corei7@gmail.com>
2020-05-15 02:49:54 +0900

    悲しみのTLE

--- ABC160-D.go
+++ ABC160-D.go
@@ -12,32 +12,44 @@ func main() {
 	sc.Split(bufio.ScanWords)
 
 	N, X, Y := nextInt(sc), nextInt(sc), nextInt(sc)
-
-	distance := make([][]int, N)
-	for i := 0; i < N; i++ {
-		distance[i] = make([]int, N)
-		for j := 0; j < N; j++ {
-			distance[i][j] = N  // 移動コストを最大値+1で初期化
-			if i - j == 1 {
-				// 隣接するノード同士のコストは 1
-				distance[i][j] = 1
-				distance[j][i] = 1
-			}
-		}
-		distance[i][i] = 0
-	}
-	// 隣接するノード同士のコストは 1
-	distance[X-1][Y-1] = 1
-	distance[Y-1][X-1] = 1
 	
 	// コスト計算「Warshall-Floyd法」
 	// 参考：https://qiita.com/ta-ka/items/a023a11efe17ab097433#warshall-floyd%E6%B3%95
+	distance := make([][]int, N)
 	for k := 0; k < N; k++ {
 		for i := 0; i < N; i++ {
+			if k == 0 {
+				distance[i] = make([]int, N)
+				if i == (X-1) {
+					distance[i][Y-1] = 1
+				}
+				if i == (Y-1) {
+					distance[i][X-1] = 1
+				}
+				if i > 0 {
+					distance[i][i-1] = 1
+				}
+				if i < N-1 {
+					distance[i][i+1] = 1
+				}
+			}
 			for j := 0; j < N; j++ {
-				if i < j {
+				if k == 0 {
+					isSetedDistance := false
+					isSetedDistance = isSetedDistance || i == j
+					isSetedDistance = isSetedDistance || i == (X - 1) && j == (Y - 1)
+					isSetedDistance = isSetedDistance || j == (X - 1) && i == (Y - 1)
+					isSetedDistance = isSetedDistance || i == (j + 1) || i == (j - 1)
+					isSetedDistance = isSetedDistance || j == (i + 1) || j == (i - 1)
+					if !isSetedDistance {	
+						distance[i][j] = N - 1
+					}
+				}
+				if i > j {
 					distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
 					distance[j][i] = distance[i][j]
+				} else {
+					break
 				}
 			}
 		}
@@ -48,7 +60,7 @@ func main() {
 	for i := 0; i < N; i++ {
 		for j := 0; j < N; j++ {
 			if i < j {
-				count[distance[i][j] - 1] += 1
+				count[distance[i][j]-1] += 1
 			}
 		}
 	}
